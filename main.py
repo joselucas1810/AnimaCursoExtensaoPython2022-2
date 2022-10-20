a51a5c0811ab3f81d28a38f2ff203074f212968a
joselucas1810 <111397299+joselucas1810@users.noreply.github.com>
2022-10-20 00:49:09 +0000

    Guardei nome e idade e exibi nome na tela

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # Meu primeiro projeto python!!!
+#
+# print () = comando de saida
 print ("Alo mundo!!")
+#quando quiser guardar uma string (frase)
+nome="Jose Lucas"
+#quando quiser guardar um numero inteiro
+idade=28
+#exibir o nome dentro da variável
+print (nome)
